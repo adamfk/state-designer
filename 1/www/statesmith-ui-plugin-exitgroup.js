e43f117425d029fdac479ee63c3cd4acb9a26b92
adam fraser-kruck <adamfk@gmail.com>
2023-01-06 03:31:39 -1000

    addCustomExitGroupHandler

--- 1/www/statesmith-ui-plugin-exitgroup.js
+++ 1/www/statesmith-ui-plugin-exitgroup.js
@@ -2,7 +2,7 @@ Draw.loadPlugin(function(ui) {
 	let graph = ui.editor.graph;
     // graph.allowDanglingEdges = true;
     graph.constrainChildren = true;     //prevent children from being outside of parent group
-    graph.extendParentsOnAdd = true;   //see issue #1
+    graph.extendParentsOnAdd = false;   //see issue #1
     graph.keepEdgesInForeground = true; //prevent edges from being behind vertices. see issue #5
 
     let toolbar = ui.toolbar;
@@ -11,8 +11,63 @@ Draw.loadPlugin(function(ui) {
     elts[0].setAttribute('title', mxResources.get('enterGroup') + ' (' + toolbar.editorUi.actions.get('enterGroup').shortcut + ')');
     elts[1].setAttribute('title', mxResources.get('exitGroup') + ' (' + toolbar.editorUi.actions.get('exitGroup').shortcut + ')');
 
+    addCustomExitGroupHandler();
     enableCustomDoubleClickHandler(graph);
 });
+
+
+function addCustomExitGroupHandler()
+{
+    let originalExitGroup = mxGraph.prototype.exitGroup;
+    mxGraph.prototype.exitGroup = function() {
+        let group = this.getCurrentRoot();
+        if (this.isValidRoot(group))
+        {
+            //remember `this` will be of type `mxGraph`
+            originalExitGroup.apply(this, arguments);
+            fitExpandedGroupToChildren(this, group);
+        }
+    };
+}
+
+
+/**
+ * Will ignore collapsed groups.
+ * @param {mxGraph} graph 
+ * @param {mxCell} group 
+ */
+function fitExpandedGroupToChildren(graph, group)
+{
+    if (!group)
+        return;
+
+    //don't adjust size for collapsed groups
+    if (group.isCollapsed())
+        return;
+
+    if (graph.getModel().getChildCount(group) <= 0)
+        return;
+
+    let geo = graph.getCellGeometry(group);
+
+    if (geo == null)
+        return;
+
+    let children = graph.getChildCells(group, true, true);
+    let includeEdges = false;
+    let kidsBoundingBox = graph.getBoundingBoxFromGeometry(children, includeEdges); // todo low - include edges that are fully contained within group
+
+    const groupBorderSize = 20;
+    let requiredWidth = kidsBoundingBox.x + kidsBoundingBox.width + groupBorderSize;
+    let requiredHeight = kidsBoundingBox.y + kidsBoundingBox.height + groupBorderSize;
+
+    geo = geo.clone(); // needed for undo support
+    let parentBoundingBox = graph.getBoundingBoxFromGeometry([group].concat(children), true);
+    geo.width = Math.max(parentBoundingBox.width, requiredWidth);
+    geo.height = Math.max(parentBoundingBox.height, requiredHeight);
+
+    graph.getModel().setGeometry(group, geo);
+}
 
 /**
  * override Graph.dblClick to support entering group on body double click issue #4
